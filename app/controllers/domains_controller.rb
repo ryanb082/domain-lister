8076dab4dfa05580409b18794bf4360fab37c9ee
Ryan Bent <ryanbent01@gmail.com>
2015-08-23 03:12:34 +0000

    Add bootsrap and table for index

--- app/controllers/domains_controller.rb
+++ app/controllers/domains_controller.rb
@@ -1,17 +1,33 @@
 class DomainsController < ApplicationController
 
   def index
+    @domains = Domain.all
   end
 
   def create
+  @domain = Domain.new(domain_params)
+
+    if @domain.save
+      flash[:notice] = "Your domain was listed"
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def new
-  	@domain = Domain.new
+    @domain = Domain.new
+
     
   end
 
   def update
+  end
+
+  private
+
+  def domain_params
+    params.require(:domain).permit(:majestic_topic, :site_topic, :links)
   end
 
 end
